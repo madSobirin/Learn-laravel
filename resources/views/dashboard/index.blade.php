a7f2a536cc6b07b80b14b85f86d0dcf5c8331d38
madSobirin <gamingbrn202@gmail.com>
2025-08-06 14:07:17 +0700

    mencoba saja

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -26,6 +26,7 @@
                             <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
                             <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
                             <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Reports</a>
+{{--                            coba--}}
                         </div>
                     </div>
                 </div>
